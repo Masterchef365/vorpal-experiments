78b1f3b090b448459f1dc01eef3cc1bd437c6014
Masterchef365 <duncan.freeman1@gmail.com>
2023-11-03 00:09:22 -0700

    Fluid sim

--- src/lib.rs
+++ src/lib.rs
@@ -4,23 +4,27 @@ use array2d::Array2D;
 mod array2d;
 use rand::prelude::*;
 
+use fluid::{FluidSim, SmokeSim};
+
+mod fluid;
+
 #[link(wasm_import_module = "kernel")]
 extern "C" {
-    fn kernel(ptr: *mut f32, width: f32, height: f32, x: f32, y: f32, time: f32);
+    fn kernel(ptr: *mut f32, width: f32, height: f32, x: f32, y: f32, time: f32, cursor_x: f32, cursor_y: f32);
 }
 
-pub fn call_kernel(width: f32, height: f32, x: f32, y: f32, time: f32) -> [f32; 4] {
+pub fn call_kernel(width: f32, height: f32, x: f32, y: f32, time: f32, cursor_x: f32, cursor_y: f32) -> [f32; 4] {
     let mut out_data = [0_f32; 4];
 
     unsafe {
-        kernel(out_data.as_mut_ptr(), width, height, x, y, time);
+        kernel(out_data.as_mut_ptr(), width, height, x, y, time, cursor_x, cursor_y);
     }
 
     out_data
 }
 
 #[no_mangle]
-pub extern "C" fn make_image(width: u32, height: u32, time: f32) -> *const f32 {
+pub extern "C" fn make_image(width: u32, height: u32, time: f32, cursor_x: f32, cursor_y: f32) -> *const f32 {
     thread_local! {
         static BUFFER: RefCell<Option<Plugin>> = RefCell::new(None);
     }
@@ -29,97 +33,99 @@ pub extern "C" fn make_image(width: u32, height: u32, time: f32) -> *const f32 {
         let mut maybe_plugin = buffer.borrow_mut();
         let plugin = maybe_plugin.get_or_insert_with(|| Plugin::new(width, height));
 
-        plugin.get_image(time).as_ptr()
+        plugin.get_image(time, cursor_x, cursor_y).as_ptr()
     })
 }
 
 struct Plugin {
-    output_buf: Array2D<[f32; 4]>,
-    sim: Sim,
+    out_rgba: Vec<f32>,
+    out_width: u32,
+    out_height: u32,
+
+    smoke_sim: SmokeSim,
+    fluid_sim: FluidSim,
+
+    last_cursor_x: f32,
+    last_cursor_y: f32,
 }
 
 impl Plugin {
     pub fn new(out_width: u32, out_height: u32) -> Self {
+        assert_eq!(out_width, out_height);
+        let w = out_width as usize;
+        let fluid_sim = FluidSim::new(w, w);
+        let mut smoke_sim = SmokeSim::new(w, w);
+
+        let intensity = 1e4;
+        smoke_sim.smoke_mut()[(w / 2, w / 3)] = intensity;
+
         Self {
-            output_buf: Array2D::new(out_width as _, out_height as _),
-            sim: Sim::new(out_width as _, out_height as _),
+            out_rgba: vec![0_f32; (out_width * out_height * 4) as usize],
+            out_width,
+            out_height,
+
+            fluid_sim,
+            smoke_sim,
+
+            last_cursor_x: -1.0,
+            last_cursor_y: -1.0,
         }
     }
 
-    pub fn get_image(&mut self, time: f32) -> &[f32] {
-        self.sim.step(time);
-        self.output_buf = self.sim.draw();
+    pub fn get_image(&mut self, time: f32, cursor_x: f32, cursor_y: f32) -> &[f32] {
+        // Draw smoke and push fluid
+        /*
+        let d = self.smoke_sim.smoke_mut();
+        let center = (d.width() / 2, d.height() / 2);
 
-        bytemuck::cast_slice(self.output_buf.data())
-    }
-}
+        let (u, v) = self.fluid_sim.uv_mut();
 
-struct Sim {
-    cells: Array2D<f32>,
-    coords: Array2D<(usize, usize)>,
-    rng: SmallRng,
-}
+        let pos = center;
+        //let time = 3. * PI / 2.;
+        u[pos] = -450. * (time).cos();
+        v[pos] = -450. * (time).sin();
+        */
 
-impl Sim {
-    pub fn new(w: usize, h: usize) -> Self {
-        let mut cells = Array2D::new(w, h);
+        // Move fluid and smoke
+        let dt = 1e-2;
+        let overstep = 1.9;
 
-        for y in 0..cells.height() {
-            for x in 0..cells.width() {
-                let [sx, sy] = [x, y].map(|v| v as f32);
+        self.fluid_sim.step(dt, overstep, 15);
+        self.smoke_sim.advect(self.fluid_sim.uv(), dt);
 
-                let rgba = call_kernel(cells.width() as f32, cells.height() as f32, sx, sy, 0.);
-
-                cells[(x, y)] = rgba[0];
-            }
-        }
-
-        let coords = coord_array(&cells);
-        Self {
-            coords,
-            cells,
-            rng: SmallRng::seed_from_u64(0xDEAD_BEEF_C0DE_D1CC),
-        }
-    }
-
-    pub fn step(&mut self, _time: f32) {
-        self.coords.data_mut().shuffle(&mut self.rng);
-        let (w, h) = (self.coords.width(), self.coords.height());
-
-        let next_frame = self
-            .coords
+        self.out_rgba = self
+            .smoke_sim
+            .smoke()
             .data()
             .iter()
-            .zip(all_coords(w, h))
-            .map(|(&scramble, origin)| {
-                let (x1, y1) = origin;
-                let (x2, y2) = scramble;
-
-                let (dx, dy) = (x1 as f32 - x2 as f32, y1 as f32 - y2 as f32);
-                let dist = (dx.powi(2) + dy.powi(2)).sqrt();
-                let dist = dist / 5000.;
-
-                let k = 1. / (dist + 1.);
-                self.cells[origin] * k + self.cells[scramble] * (1. - k)
-            })
+            .map(|v| [*v; 4])
+            .flatten()
             .collect();
-        self.cells = Array2D::from_array(self.coords.width(), next_frame);
+        /*
+        // Make image
+        for y in 0..self.out_height {
+            for x in 0..self.out_width {
+                let rgba = call_image_fn(self.out_width as f32, self.out_height as f32, x as f32, y as f32, time);
+
+                let base = (x * 4 + y * self.out_width * 4) as usize;
+                self.out_rgba[base..base + 4].copy_from_slice(&rgba);
+            }
+        }
+        */
+
+        if self.last_cursor_x != -1.0 && cursor_x != -1.0 {
+            let delta_x = cursor_x - self.last_cursor_x;
+            let delta_y = cursor_y - self.last_cursor_y;
+
+            let (u, v) = self.fluid_sim.uv_mut();
+            let pos = (cursor_x as usize, cursor_y as usize);
+            u[pos] += delta_x;
+            v[pos] += delta_y;
+        }
+
+        self.last_cursor_x = cursor_x;
+        self.last_cursor_y = cursor_y;
+
+        &self.out_rgba
     }
-
-    pub fn draw(&self) -> Array2D<[f32; 4]> {
-        map_array(&self.cells, |&val| [val, val, val, 1.])
-    }
-}
-
-fn map_array<T, U>(arr: &Array2D<T>, f: impl FnMut(&T) -> U) -> Array2D<U> {
-    Array2D::from_array(arr.width(), arr.data().iter().map(f).collect())
-}
-
-fn coord_array<T>(arr: &Array2D<T>) -> Array2D<(usize, usize)> {
-    let data = all_coords(arr.width(), arr.height()).collect();
-    Array2D::from_array(arr.width(), data)
-}
-
-fn all_coords(w: usize, h: usize) -> impl Iterator<Item = (usize, usize)> {
-    (0..h).map(move |y| (0..w).map(move |x| (x, y))).flatten()
 }
