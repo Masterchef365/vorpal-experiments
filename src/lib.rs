cbdf5fc6bc07cd6837531aabd585e89973f242ba
Masterchef365 <duncan.freeman1@gmail.com>
2023-11-03 00:15:32 -0700

    Cursor and fluid interact

--- src/lib.rs
+++ src/lib.rs
@@ -10,21 +10,53 @@ mod fluid;
 
 #[link(wasm_import_module = "kernel")]
 extern "C" {
-    fn kernel(ptr: *mut f32, width: f32, height: f32, x: f32, y: f32, time: f32, cursor_x: f32, cursor_y: f32);
+    fn kernel(
+        ptr: *mut f32,
+        width: f32,
+        height: f32,
+        x: f32,
+        y: f32,
+        time: f32,
+        cursor_x: f32,
+        cursor_y: f32,
+    );
 }
 
-pub fn call_kernel(width: f32, height: f32, x: f32, y: f32, time: f32, cursor_x: f32, cursor_y: f32) -> [f32; 4] {
+pub fn call_kernel(
+    width: f32,
+    height: f32,
+    x: f32,
+    y: f32,
+    time: f32,
+    cursor_x: f32,
+    cursor_y: f32,
+) -> [f32; 4] {
     let mut out_data = [0_f32; 4];
 
     unsafe {
-        kernel(out_data.as_mut_ptr(), width, height, x, y, time, cursor_x, cursor_y);
+        kernel(
+            out_data.as_mut_ptr(),
+            width,
+            height,
+            x,
+            y,
+            time,
+            cursor_x,
+            cursor_y,
+        );
     }
 
     out_data
 }
 
 #[no_mangle]
-pub extern "C" fn make_image(width: u32, height: u32, time: f32, cursor_x: f32, cursor_y: f32) -> *const f32 {
+pub extern "C" fn make_image(
+    width: u32,
+    height: u32,
+    time: f32,
+    cursor_x: f32,
+    cursor_y: f32,
+) -> *const f32 {
     thread_local! {
         static BUFFER: RefCell<Option<Plugin>> = RefCell::new(None);
     }
@@ -101,27 +133,42 @@ impl Plugin {
             .map(|v| [*v; 4])
             .flatten()
             .collect();
-        /*
-        // Make image
-        for y in 0..self.out_height {
-            for x in 0..self.out_width {
-                let rgba = call_image_fn(self.out_width as f32, self.out_height as f32, x as f32, y as f32, time);
 
-                let base = (x * 4 + y * self.out_width * 4) as usize;
-                self.out_rgba[base..base + 4].copy_from_slice(&rgba);
+        // Make image
+        if self.last_cursor_x != -1.0 && cursor_x != -1.0 {
+            for y in 0..self.out_height {
+                for x in 0..self.out_width {
+                    let rgba = call_kernel(
+                        self.out_width as f32,
+                        self.out_height as f32,
+                        x as f32,
+                        y as f32,
+                        time,
+                        cursor_x,
+                        cursor_y,
+                    );
+
+                    let delta_x = cursor_x - self.last_cursor_x;
+                    let delta_y = cursor_y - self.last_cursor_y;
+
+                    let (u, v) = self.fluid_sim.uv_mut();
+                    let pos = (x as usize, y as usize);
+                    u[pos] += rgba[0] * delta_x;
+                    v[pos] += rgba[0] * delta_y;
+                }
             }
         }
-        */
 
+        /*
         if self.last_cursor_x != -1.0 && cursor_x != -1.0 {
             let delta_x = cursor_x - self.last_cursor_x;
             let delta_y = cursor_y - self.last_cursor_y;
 
-            let (u, v) = self.fluid_sim.uv_mut();
             let pos = (cursor_x as usize, cursor_y as usize);
             u[pos] += delta_x;
             v[pos] += delta_y;
         }
+        */
 
         self.last_cursor_x = cursor_x;
         self.last_cursor_y = cursor_y;
